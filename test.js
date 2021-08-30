67bc05939b619eb54382196eae5615bd8636bff3
Charles L <charles.lobo@gmail.com>
2021-08-30 10:07:19 +1000

    feat: add tester

---
+++ test.js
@@ -0,0 +1,14 @@
+'use strict'
+const express = require('express')
+const app = express()
+
+const logger = require('./')
+
+app.use(logger())
+
+app.get('/', (req, res) => {
+  res.send("Hello world!")
+})
+
+const port = 3434
+app.listen(port, () => console.log(`Started on port: ${port}`))
